0d67236515944110938f8cd6f22fbd4b2e169b21
Maria Fisher <mariacha@gmail.com>
2016-12-13 17:14:21 -0800

    Adding robo helper function. Enabling all migrate modules.

--- RoboFile.php
+++ RoboFile.php
@@ -9,5 +9,19 @@
 class RoboFile extends \ThinkShout\RoboDrupal\Tasks
 {
 
+  /**
+   * Cleanup migrations.
+   *
+   * @option string migrations
+   *   Optional list of migrations to reset, separated by commmas.
+   */
+  public function migrateCleanup($opts = ['migrations' => '']) {
+    $migrations = explode(',', $opts['migrations']);
+    $project_properties = $this->getProjectProperties();
+    foreach ($migrations as $migration) {
+      $this->taskExec('drush mrs ' . $migration)->dir($project_properties['web_root'])->run();
+    }
+    $this->taskExec('drush mr --all && drush pmu migrate_google_sheets_example -y && drush en migrate_google_sheets_example -y && drush ms')->dir($project_properties['web_root'])->run();
+  }
 
 }
